0d6bb156272c7ea315491bd8530c6e46820bdf9b
Akash Gautam <gautamankush265@gmail.com>
2023-03-20 22:51:59 +0530

    selenium upgrade.

---
+++ cypress.config.js
@@ -0,0 +1,31 @@
+const { defineConfig } = require("cypress");
+const getCompareSnapshotsPlugin = require('cypress-image-diff-js/dist/plugin');
+
+module.exports = defineConfig({
+  "env": {
+    "preserveOriginalScreenshot": true,
+    "supportFile": false,
+  },
+  defaultCommandTimeout: 20000,
+  responseTimeout: 20000,
+  requestTimeout: 20000,
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    "reportDir": "cypress/reports",
+    "charts": true,
+    "reportPageTitle": "GJG-Cypress",
+    "reportFilename": "GJG_Report",
+    "embeddedScreenshots": true,
+    "inlineAssets": true,
+    "saveAllAttempts": true,
+  },
+  e2e: {
+    setupNodeEvents(on, config) {
+      getCompareSnapshotsPlugin(on, config);
+      require('cypress-mochawesome-reporter/plugin')(on);
+    },
+  },
+});
+
+
+require('@applitools/eyes-cypress')(module);
